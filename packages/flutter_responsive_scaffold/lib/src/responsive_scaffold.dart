5c3db5c7257e8638d326074966af98549320b5b8
Sushmoy Roy <sushmoyr@gmail.com>
2023-08-06 14:06:44 +0600

    added the ability to limit max scaffold width

--- packages/flutter_responsive_scaffold/lib/src/responsive_scaffold.dart
+++ packages/flutter_responsive_scaffold/lib/src/responsive_scaffold.dart
@@ -25,6 +25,7 @@ class ResponsiveScaffold extends StatelessWidget {
     this.scaffoldKey,
     this.gutterSpacing = 24,
     this.contentPadding = const EdgeInsets.all(20),
+    this.maxScaffoldConstraints,
   }) : super(key: key);
 
   // All the window panes
@@ -49,6 +50,7 @@ class ResponsiveScaffold extends StatelessWidget {
   final bool? extendBodyBehindAppbar;
   final double gutterSpacing;
   final EdgeInsets contentPadding;
+  final BoxConstraints? maxScaffoldConstraints;
 
   @override
   Widget build(BuildContext context) {
@@ -121,47 +123,54 @@ class ResponsiveScaffold extends StatelessWidget {
       ],
     );
 
-    return Scaffold(
-      key: scaffoldKey,
-      backgroundColor: backgroundColor,
-      floatingActionButton: floatingActionButton,
-      appBar: topNavigationBar,
-      bottomNavigationBar: bottomNavigationBar,
-      drawer: drawer,
-      endDrawer: endDrawer,
-      body: SizedBox.expand(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            /// Left navigation pane
-            AnimatedSize(
-              duration: kThemeChangeDuration,
-              alignment: Alignment.topLeft,
-              child: sideNavigation ?? const SizedBox.shrink(),
-            ),
-
-            /// Body area pane
-            Expanded(
-              child: Padding(
-                padding: contentPadding,
-                child: Column(
-                  children: [
-                    // First the bottom widgets
-                    bottomWidgets,
-                    // Then the body widgets
-                    Expanded(child: bodyWidgets)
-                  ],
+    return Material(
+      color: backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
+      child: ConstrainedBox(
+        constraints: maxScaffoldConstraints ?? const BoxConstraints.expand(),
+        child: Scaffold(
+          key: scaffoldKey,
+          backgroundColor:
+              backgroundColor ?? Theme.of(context).scaffoldBackgroundColor,
+          floatingActionButton: floatingActionButton,
+          appBar: topNavigationBar,
+          bottomNavigationBar: bottomNavigationBar,
+          drawer: drawer,
+          endDrawer: endDrawer,
+          body: SizedBox.expand(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                /// Left navigation pane
+                AnimatedSize(
+                  duration: kThemeChangeDuration,
+                  alignment: Alignment.topLeft,
+                  child: sideNavigation ?? const SizedBox.shrink(),
                 ),
-              ),
-            ),
 
-            /// Right navigation pane
-            AnimatedSize(
-              duration: kThemeChangeDuration,
-              alignment: Alignment.topRight,
-              child: endSideNavigation ?? const SizedBox.shrink(),
+                /// Body area pane
+                Expanded(
+                  child: Padding(
+                    padding: contentPadding,
+                    child: Column(
+                      children: [
+                        // First the bottom widgets
+                        bottomWidgets,
+                        // Then the body widgets
+                        Expanded(child: bodyWidgets)
+                      ],
+                    ),
+                  ),
+                ),
+
+                /// Right navigation pane
+                AnimatedSize(
+                  duration: kThemeChangeDuration,
+                  alignment: Alignment.topRight,
+                  child: endSideNavigation ?? const SizedBox.shrink(),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
